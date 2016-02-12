c605e83799c571df1b3e3abaf3a6f0bbbf744366
Nate Parrish <nate.parrish@arrowpointe.com>
2016-02-11 20:07:18 -0800

    Implement test message to server from client

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-var server = http.createServer(app)
-server.listen(port)
+var server = http.createServer(app);
+server.listen(port);
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+var wss = new WebSocketServer({server: server});
+console.log("websocket server created");
 
 wss.on("connection", function(ws) {
   var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    ws.send(JSON.stringify(new Date()), function() {  });
+  }, 1000);
 
-  console.log("websocket connection open")
+  console.log("websocket connection open");
 
   ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+    console.log("websocket connection close");
+    clearInterval(id);
+  });
+
+  ws.on("message", function(data){
+  	console.log(data);
+  });
+});
